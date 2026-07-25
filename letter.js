6486b28018e329dc528a8701e9f2e4d963a07996
Tokom7193 <riijinp@gmail.com>
2026-07-25 17:22:12 +0530

    Update letter.js

--- letter.js
+++ letter.js
@@ -1,1 +1,13 @@
+const envelope = document.getElementById("envelope");
 
+let opened = false;
+
+function openEnvelope(){
+
+    if(opened) return;
+
+    opened = true;
+
+    envelope.classList.add("open");
+
+}
